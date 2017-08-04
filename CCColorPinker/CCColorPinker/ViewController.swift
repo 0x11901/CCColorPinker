0279844d980818adde6e0d626875b115ceb31e38
王靖凯 <wjk930726@live.com>
2017-08-05 00:08:08 +0800

    简单布局，添加了一个点击事件与蒙板效果

--- CCColorPinker/CCColorPinker/ViewController.swift
+++ CCColorPinker/CCColorPinker/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorVIew: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func modalColorPinker(_ sender: UIButton) {
+        //蒙板效果
+        let blurView = UIToolbar.init(frame: self.view.bounds)
+        blurView.barStyle = .blackTranslucent
+        self.view.addSubview(blurView)
+        
+    }
+    
 }
 
